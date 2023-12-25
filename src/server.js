ace6ccb9b7a02257e75766c1dcfacce8ec18102f
minatisleeping <minatt2002@gmail.com>
2023-12-25 12:28:35 +0700

    init code base

--- src/server.js
+++ src/server.js
@@ -1,15 +1,27 @@
-// const express = require('express')
 import express from 'express'
+import { mapOrder } from '~/utils/sorts.js'
 
 const app = express()
+
 const hostname = 'localhost'
 const port = 8017
 
-
-app.get('/', function (req, res) {
-  res.send(`<h1>helooo Nodejs. I'm Minatisleeping</h1>`)
+app.get('/', (req, res) => {
+  // Test Absolute import mapOrder
+  // eslint-disable-next-line no-console
+  console.log(mapOrder(
+    [{ id: 'id-1', name: 'One' },
+      { id: 'id-2', name: 'Two' },
+      { id: 'id-3', name: 'Three' },
+      { id: 'id-4', name: 'Four' },
+      { id: 'id-5', name: 'Five' }],
+    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+    'id'
+  ))
+  res.end('<h1>Hello World!</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
-    console.log(`Helloo MinatDev, I'm running server at http://${hostname}:${port}/`)
+  // eslint-disable-next-line no-console
+  console.log(`Hello Trung Quan Dev, I am running at http://${ hostname }:${ port }/`)
 })
