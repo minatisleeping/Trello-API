2bfbf2ec6b33f7557099a3c02a7c61284f273a2f
minatisleeping <minatt2002@gmail.com>
2024-01-23 16:04:36 +0700

    Using Joi to validate data

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
-import { boardRoutes } from '~/routes/v1/boardRoutes'
+import { boardRoute } from '~/routes/v1/boardRoute'
 
 const Router = express.Router()
 
@@ -12,7 +12,7 @@ Router.get('/status', (req, res) => {
 })
 
 /** Board APIs */
-Router.use('/boards', boardRoutes)
+Router.use('/boards', boardRoute)
 
 export const API_V1 = Router // cách này thì dùng object destructuring (có dạng { .. } )
 // export default Router
--- src/server.js
+++ src/server.js
@@ -8,6 +8,10 @@ import { API_V1 } from '~/routes/v1'
 const START_SERVER = () => {
   const app = express()
 
+  // Enable req.body json data
+  app.use(express.json())
+
+  // Use APIs V1
   app.use('/v1', API_V1)
 
   app.listen(env.APP_PORT, env.APP_HOST, () => {
