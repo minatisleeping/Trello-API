a744c4f505de71903382b9bd85c474d15da5d72b
minatisleeping <minatt2002@gmail.com>
2024-01-24 08:09:07 +0700

    Understanding env dev & production

--- src/config/environment.js
+++ src/config/environment.js
@@ -3,8 +3,11 @@ import 'dotenv/config'
 export const env = {
   MONGODB_URI: process.env.MONGODB_URI,
   DATABASE_NAME: process.env.DATABASE_NAME,
+
   APP_HOST: process.env.APP_HOST,
   APP_PORT: process.env.APP_PORT,
+
+  BUILD_MODE: process.env.BUILD_MODE,
 
   AUTHOR: process.env.AUTHOR
 }
